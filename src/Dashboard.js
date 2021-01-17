bb95f083ea9be11f2ff47eda437842d59c4590a0
Haeim Lee <h283lee@uwaterloo.ca>
2021-01-17 02:10:35 -0500

    add login and dashboard design

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,6 +7,7 @@ import { logOut } from "./services/firebase";
 export default function Dashboard() {
   const user = useContext(UserContext);
   const [redirect, setredirect] = useState(null);
+  var todaysChallenges = "4";
 
   useEffect(() => {
     if (!user) {
@@ -19,14 +20,57 @@ export default function Dashboard() {
 
   return (
     <div className="dashboard">
-      <h1 className="dashboard-text">Welcome Home</h1>
-      <button className="logout-button" onClick={logOut}>
-        <img
-          src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
-          alt="google icon"
-        />
-        <span> logout</span>
-      </button>
+      <div className="relativeWrapperTwo">
+          <button className="dashboardLogoutCircle" onClick={logOut}>
+            Logout
+          </button>
+      </div>
+      <p className="dashboardTitle">
+        See You Soon
+      </p>
+      <p className="dashboardSubtitle">
+        We will be sending challenges through notifications during the day.
+      </p>
+      <div className="dashboardRelativeWrapperOne">
+        <div className="icons8Seesaw501" />
+        <div className="hand">
+          <img
+            alt=""
+            className="hand1"
+            src="https://static.overlay-tech.com/assets/79c1ab0e-008e-4a35-a637-dc7b14663f7f.svg"
+          />
+          <img
+            alt=""
+            className="hand2"
+            src="https://static.overlay-tech.com/assets/f7ea3a7e-4b47-4dc9-b4b1-847c41619cf8.svg"
+          />
+          <img
+            alt=""
+            className="hand3"
+            src="https://static.overlay-tech.com/assets/a575a311-692e-497b-b2f0-5fff0b2679ae.svg"
+          />
+          <img
+            alt=""
+            className="hand4"
+            src="https://static.overlay-tech.com/assets/8294bd34-c834-4c1c-8784-8c253be65821.svg"
+          />
+          <img
+            alt=""
+            className="hand5"
+            src="https://static.overlay-tech.com/assets/5ae3fdc5-5742-41cb-9ad7-27b3e0cdacd8.svg"
+          />
+        </div>
+      </div>
+      <div className="dashboarFlexWrapperOne">
+        <p className="todayNumChallenges">
+          Today's number of challenges: 
+        </p>
+        <div className="numChallengesCir">
+          <p className="todayChallenges">
+            {todaysChallenges}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -7,41 +7,90 @@ import firebase from 'firebase/app';
 import { getDate } from './Helpers';
 
 export default function Login() {
-  const user = useContext(UserContext)
-  const [redirect, setredirect] = useState(null)
+    const user = useContext(UserContext)
+    const [redirect, setredirect] = useState(null)
 
-  useEffect(() => {
-    if (user) {
-        var user_id = firebase.auth().currentUser.uid;
-        firebase.database().ref('users/'+user_id).once('value').then(userSnapshot => {
-            var children = userSnapshot.val();
-            if(userSnapshot.exists()){
-                //user exists
-                var dateToday = getDate(children.year, children.month, children.day);
-                if(dateToday){
-                    setredirect('/dashboard');
-                } else {
+    useEffect(() => {
+        if (user) {
+            var user_id = firebase.auth().currentUser.uid;
+            firebase.database().ref('users/'+user_id).once('value').then(userSnapshot => {
+                var children = userSnapshot.val();
+                if(userSnapshot.exists()){
+                    //user exists
+                    var dateToday = getDate(children.year, children.month, children.day);
+                    if(dateToday){
+                        setredirect('/dashboard');
+                    } else {
+                        setredirect('/feeling');
+                    }
+                } else { 
+                    //new user
+                    addUser(user_id);
                     setredirect('/feeling');
                 }
-            } else { 
-                //new user
-                addUser(user_id);
-                setredirect('/feeling');
-            }
-        }).catch(error => {
-            console.error(error);
-        });
+            }).catch(error => {
+                console.error(error);
+            });
+        }
+    }, [user])
+    if (redirect) {
+        return <Redirect to={redirect}/>
     }
-  }, [user])
-  if (redirect) {
-    return <Redirect to={redirect}/>
-  }
-  return (
-      <div className="login-buttons">
-        <button className="login-provider-button" onClick={signInWithGoogle}>
-        <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
-        <span> Continue with Google</span>
-       </button>
-      </div>
-  );
+    return (
+        <div className="page20">
+            <p className="welcomeToBalanced">
+                Welcome To <br /> Balanced
+            </p>
+            <div className="relativeWrapperOne">
+                <div className="icons8Seesaw501"/>
+                <div className="balanceBall21">
+                    <div className="seesaw">
+                        <img
+                        alt=""
+                        className="vector"
+                        src="https://static.overlay-tech.com/assets/5db8f63d-f704-43e5-8ec9-ff725493488a.svg"
+                        />
+                        <img
+                        alt=""
+                        className="circle2"
+                        src="https://static.overlay-tech.com/assets/cebb993c-0c8b-4125-a90c-8671a1b37196.svg"
+                        />
+                        <img
+                        alt=""
+                        className="line1"
+                        src="https://static.overlay-tech.com/assets/20a83157-949f-4d4b-9468-407dfff643fb.svg"
+                        />
+                        <img
+                        alt=""
+                        className="line2"
+                        src="https://static.overlay-tech.com/assets/35f667cf-29c3-4284-a0f9-d7cdd2c80933.svg"
+                        />
+                        <img
+                        alt=""
+                        className="line3"
+                        src="https://static.overlay-tech.com/assets/6d2aceec-0bee-4e27-a601-192ef10f488f.svg"
+                        />
+                        <img
+                        alt=""
+                        className="line4"
+                        src="https://static.overlay-tech.com/assets/b6269dab-e9b9-4e62-90d2-588b360a260d.svg"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="relativeWrapperOne">
+                <button className="flexWrapperOne" onClick={signInWithGoogle}>
+                    <img
+                        alt=""
+                        src="https://static.overlay-tech.com/assets/51d06d55-9eaf-40ca-b8aa-99b2ded127fa.png"
+                    />
+                    <p className="signInButton">
+                        Sign in with gmail
+                    </p>
+                    
+                </button>
+                <p className="subtitle">A customized approach to maintaining a work - life balance.</p>
+            </div>
+        </div>
+    );
 }
